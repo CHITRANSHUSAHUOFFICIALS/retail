9276270737745c96164500587f5db4e8b2785dd8
CHITRANSHUSAHUOFFICIALS <sahuchitranshu2000@gmail.com>
2025-02-11 23:33:34 +0530

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './core/pages/login/login.component';
+import { DashboardComponent } from './shared/layout/dashboard/dashboard.component';
+import { authGuard } from './config/auth-guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: DashboardComponent, children: [], canActivate: [authGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  logOut() {
+
+  }
+
+  performLogoutAction() {
+    //clear localstorage
+    //redirect to login page
+  }
+}
